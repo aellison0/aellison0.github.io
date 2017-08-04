6029c08d4db547a9a4df0b363d4df12440b80871
aellison0 <aellison32.ae@gmail.com>
2017-08-04 14:59:31 -0400

    yes

--- germany-world-cup/scripts.js
+++ germany-world-cup/scripts.js
@@ -1,8 +1,18 @@
-$(document).ready (function() {
-$("#legendhide").click(function(){
-  $(".legendblock").toggle();
-  $("#show").toggle();
-  $("#hide").toggle();
-  $("#legendhide").toggleClass("btn-primary btn-danger");
+$(document).ready(function(){
+	$("#legendhide").click(function(){
+		$(".legendblock").toggle();
+		$("#show,#hide").toggle();
+		$("#legendhide").toggleClass("btn-prmary btn-danger");
+	});
+$("#togglequarterfinalist").click(function(){
+	$("#hideteams,#showteams").toggle();
+	var series=chart.series[1];
+	if (series.visibl){
+		series.hide();
+	}
+	else {
+		series.show();
+	}
+	$("#togglequarterfinalist").toggleClass("btn-prmary btn-danger");
 });
 });
