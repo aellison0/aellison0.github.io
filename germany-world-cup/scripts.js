5518c22bff8c16b6d172e898ec1e74bfe9c89c65
aellison0 <aellison32.ae@gmail.com>
2017-08-04 15:06:05 -0400

    commit

--- germany-world-cup/scripts.js
+++ germany-world-cup/scripts.js
@@ -1,18 +1,19 @@
-$(document).ready(function(){
-	$("#legendhide").click(function(){
-		$(".legendblock").toggle();
-		$("#show,#hide").toggle();
-		$("#legendhide").toggleClass("btn-prmary btn-danger");
-	});
-$("#togglequarterfinalist").click(function(){
-	$("#hideteams,#showteams").toggle();
-	var series=chart.series[1];
-	if (series.visibl){
-		series.hide();
-	}
-	else {
-		series.show();
-	}
-	$("#togglequarterfinalist").toggleClass("btn-prmary btn-danger");
+$(document).ready (function() {
+$("#legendhide").click(function(){
+  $(".legendblock").toggle();
+  $("#show").toggle();
+  $("#hide").toggle();
+  $("#legendhide").toggleClass("btn-primary btn-danger");
 });
+});
+$("#togglequarterfinalists").click(function() {
+    $("#hideteams, #showteams").toggle();
+    var series = chart.series[1];
+    if(series.visible) {
+      series.hide();
+    } else {
+      series.show();
+    }
+    $("#togglequarterfinalists").toggleClass("btn-danger");
+  });
 });
